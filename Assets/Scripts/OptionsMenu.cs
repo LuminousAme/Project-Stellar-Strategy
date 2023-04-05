11c4d3385d25f21b928f4a79507c85f0b3969d18
Jackie <jzhou39@gmail.com>
2023-04-05 09:41:53 -0400

    some changes

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -1,42 +1,75 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
+using TMPro;
+
 
 public class OptionsMenu : MonoBehaviour
 {
+    [SerializeField] private TMP_Dropdown factionColorDropdown;
+    [SerializeField] private TMP_Dropdown resolutionDropdown;
+    Resolution[] resolutions;
 
-[SerializeField]    private Dropdown factionColorDropdown;
-[SerializeField] private Dropdown graphicsDropdown;
 
-[SerializeField] Slider masterVol, musicVol, sfxVol;
+    public AudioMixer audioMixer;
+
+    [SerializeField] private Slider masterVol, musicVol, sfxVol;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-   
-        // audio
-        masterVol.value = MatchManager.instance.masterVolume;
-        musicVol.value = MusicManager.instance.musicVolume;
-        sfxVol.value = MatchManager.instance.sfxVolume;
 
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        resolutions = Screen.resolutions;
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " +
+                            resolutions[i].height;
+            options.Add(option);
+            if (resolutions[i].width == Screen.currentResolution.width
+                && resolutions[i].height == Screen.currentResolution.height)
+                currentResolutionIndex = i;
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.RefreshShownValue();
+
+        // audio
+        // masterVol.value = MatchManager.instance.masterVolume;
+        // musicVol.value = MusicManager.instance.musicVolume;
+        // sfxVol.value = MatchManager.instance.sfxVolume;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         // Update UI elements based on current settings (e.g., load saved settings)
         masterVol.onValueChanged.AddListener(SetMasterVolume);
-
     }
 
     // Callback for audio settings selection
-    void SetMasterVolume(float value)
+    private void SetMasterVolume(float value)
     {
+        audioMixer.SetFloat("MasterVolume", value);
         // Get the selected audio volume level
         MatchManager.instance.masterVolume = value;
-
-
     }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width,
+            resolution.height, Screen.fullScreen);
+    }
+
 }
